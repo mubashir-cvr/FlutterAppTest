40e61848160c090d662462b04dace3c24f7c5d7c
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-22 21:40:43 +0530

    Settup root widget

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); //run app root widget
 }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
